be454d5d76aab17df2b611694e275615aa647bb4
Keisei Jia <keisei.jia@qq.com>
2019-12-09 23:19:41 +0800

    :construction: set up loader config

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,18 @@ module.exports = {
   entry: './main.js',
   output: {
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: [
+          {
+            loader: path.resolve('path/to/loader.js'),
+            options: {}
+          }
+        ]
+      }
+    ]
   }
 };
